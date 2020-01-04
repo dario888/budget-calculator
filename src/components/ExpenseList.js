8f6d4ca566f5ad8c5a5cc747cff887b98f011c77
dario888 <dario.bario87@hotmail.com>
2020-01-04 11:52:54 +0100

    Commit 2: Adding clear button

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Item from './ExpenseItem';
+import {MdDelete} from 'react-icons/md'
 
 
 
@@ -11,8 +12,11 @@ const ExpenseList = ({expenses}) => {
             {expenses.map(expense => {
                 return <Item key={expense.id} expenses={expenses} />
             })}
-            {/* <li></li> */}
         </ul>
+        {expenses.length > 0 && (<button className="btn">
+        clear expenses
+        <MdDelete className="btn-icon" />
+        </button>)}
         </>
     )
 }
